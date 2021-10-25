191fba84b85cc1d73797d67c72d4b308f231e138
rishabhrajpoot1 <rishabh1399904@gmail.com>
2021-10-25 17:03:53 +0530

    block-BJaaca

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -2,97 +2,85 @@
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
 
+let character ={};
+undefined
+
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
 
+character.characterName='Arya';
+'Arya'
+
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 
 let age = 20;
+undefined
+character["character-age"]=age;
+20
 
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
 
+character["Sur name"]="Stark";
+'Stark'
+
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
+
+character.title="Lady of Winterfell"
+'Lady of Winterfell'
 
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
 
+character.greet = function () {
+    alert(`I am ${character.characterName} and my title is ${character.title}.`);};   
+
 // - Add a new property to the character object named `isFemale` and value will be `true`
+
+character.isFemale=`true`;
+'true'
 
 // - Execute the function stored in the greet key of the character object
 
+character.greet();
+
 //  - Change the method `greet` to now alert `She is [NAME HERE] and her title is [TITLE HERE]`.
+
+character.greet = function () {
+    alert(`She is ${character.characterName} and her title is ${character.title}.`);};   
+
 
 // - Check by calling `greet` and see if the message changed or not.
 
+character.greet();
+
 //  - Add a new property in the character object `isAdult`. The value will be true if age is greater than 18 or it will be false.
 
+character.isAdult=character["character-age"]>18?true:false;
+true
+
 // - Add a new method (function inside objects are called methods) named `changeIsAdult`. When called should be able to flip the value of `isAdult` from true to false and false to true.
+
+character.changeIsAdult=function(){!character.isAdult?true:false;}
+false
 
 // - Add a new key with the name stored in variable `keyName` defined below i.e `playedBy` (use [] for computed property) and the value of "Maisie Williams"
 
 let keyName = 'playedBy';
 
+character[keyName]= "Maisie Williams";
+'Maisie Williams'
+
 // - Using `alert` alert the value stored in key you added above `keyName` use the variable name to access
+
+alert(character.keyName);
 
 // - Using the function `console.log` log the value of the key `42`. You don't have to add the key
 
+console.log(character[42]);
+
 // - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
 
-// - Access the value of the key `totalSeason` (it should be 8)
-
-
-
-
-let character ={};
-undefined
-character.characterName='Arya';
-'Arya'
-character
-{characterName: 'Arya'}
-let age=20;
-undefined
-character["character age"]=age;
-20
-character["Sur name"]="Stark";
-'Stark'
-character.title="Lady of Winterfell"
-'Lady of Winterfell'
-character.greet=function(){
-alert(`I am ${character.characterName} and my title is ${character.title}.`);}
-
-character.greet=function()
-{alert(`I am ${character.characterName} and my title is ${character.title}.`);}
-undefined
-character.isFemale=`true`;
-'true'
-character.greet
-undefined
-character
-
-character["greet"];
-undefined
-character.greet=alert(`She is ${character.characterName} and her title is ${character.title}`);
-undefined
-character.greet
-undefined
-character.isAdult=character["character-age"]>18?true:false;
-true
-character.changeIsAdult=function(){!character.isAdult?true:false;}
-false
-character["keyName"]='playedby';
-'playedby'
-let keyName='playeby';
-undefined
-alert(character.keyName);
-undefined
-console.log('42');
-
-
-console.log(character["42"]);
-
 character.totalSeasons=1+2+3+2;
-'1+2+3+2'
-character.totalSeasons
-'1+2+3+2'
-Number(character.totalSeasons);
-NaN
+
+// - Access the value of the key `totalSeason` (it should be 8)
+console.log(character.totalSeasons);
